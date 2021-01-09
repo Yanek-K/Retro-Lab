8d1c24c8c78acef98bb113f57890075faf391043
Yanek K <yanek.dev@gmail.com>
2021-01-09 11:40:15 -0500

    fixed styling issues on process page and contact

--- main.js
+++ main.js
@@ -13,3 +13,17 @@ $('.navbar a').on('click', function(e){
     }
 });
 
+/* Fade */
+
+
+function scrollAppear () {
+    var fadeIn = document.querySelector('.fade-in');
+    var position = fadeIn.getBoundingClientRect().top;
+    var screenPosition = window.innerHeight;
+
+    if(position < screenPosition){
+        fadeIn.classList.add("fade-in-appear")
+    }
+}
+
+    window.addEventListener('scroll', scrollAppear);
